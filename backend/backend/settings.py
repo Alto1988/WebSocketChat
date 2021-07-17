b56ddecffe3f1467332b0712a98a6e4966fb8c8f
Jacob Pagan <jake2415@gmail.com>
2021-07-17 16:49:37 -0500

    Finally got a connect to the redis server. Redis does not play nice with windows

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [('127.0.0.1', 6379)],
         },
