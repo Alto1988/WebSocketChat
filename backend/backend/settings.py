e81b53e47e891e3d6354b912bd7583d5f4a27112
Jacob Pagan <jake2415@gmail.com>
2021-07-17 10:34:39 -0500

    More research needed but keep getting unexpected argument 'host' when trying to connect. Code needs updating

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,8 +69,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-ASGI_APPLICATION = "backend.asgi.application"
-
+ASGI_APPLICATION = 'backend.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
